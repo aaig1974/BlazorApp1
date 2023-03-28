5fb83367c1cc9911620fca0e2ff4703053da3a63
aaig <aaig@DESKTOP-T7DAPN9>
2023-03-28 18:05:27 +0200

    Add project files.

---
+++ BlazorApp1/Models/MeldeDaten.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace BlazorApp1.Models
+{
+    public class MeldeDaten
+    {
+        public MeldeDaten(string bereich)
+        {
+            Datum = DateOnly.FromDateTime(DateTime.Now);
+            TimeOnly time = TimeOnly.FromDateTime(DateTime.Now);
+            AnmeldeZeit = time.ToString("HH:mm");
+            AbmeldeZeit = "--:--";
+            Bereich = bereich;
+        }
+        public DateOnly Datum { get; set; }
+        public string AnmeldeZeit { get; set; }
+        public string AbmeldeZeit { get; set; }
+        public string Bereich { get; set; }
+    }
+}
